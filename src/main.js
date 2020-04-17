cf8c36e56898a404292e866430b9b898c1f6ffef
GouZhangMing <damageanything@163.com>
2020-04-17 17:06:33 +0800

    push

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false
+
+import {Input} from '../node_modules/element-ui';
+
+Vue.use(Input);
+
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="inputBox">
+      <el-input v-model="message" placeholder="请输入内容"></el-input>
+      <el-input v-model="message" placeholder="请输入内容"></el-input>
+    </div>
+      
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  methods: {
+
+  },
+  computed: {
+  message: {
+    get () {
+      return this.$store.state.message
+    },
+    set (value) {
+      this.$store.commit('updateMessage', value)
+    }
+  }
+},
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#inputBox {
+  display:flex;
+  justify-content:center;
+  align-items:center
+}
+
+
+
+</style>
+
